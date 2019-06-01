2f120bcfc6eaa756cf5ff1f0859f7d9121bff1d0
nikunjAg <aggarwalnikunj1999@gmail.com>
2019-06-01 19:50:33 +0530

    Add view engine

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+app.set('view-engine', 'hbs');
+
 const PORT = 3000 || process.env.PORT;
+
 app.use(express.static(__dirname + '/'));
 console.log(__dirname);
 app.get('/', (req, res, next) => {
-    res.render('./Circles.html');
+    res.render('Circles.hbs');
 });
 app.listen(PORT, ()=> {
     console.log(`App is running on PORT ${PORT}`)
