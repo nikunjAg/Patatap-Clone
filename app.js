70a77ab9d9a40ea5023628f8ccd8c4a98fcc9666
nikunjAg <aggarwalnikunj1999@gmail.com>
2019-06-01 19:38:12 +0530

    Ready for Heroku

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
-
 app.use(express.static(__dirname + '/'));
 console.log(__dirname);
-
 app.get('/', (req, res, next) => {
     res.render('./Circles.html');
 });
-
 app.listen(PORT, ()=> {
     console.log(`App is running on PORT ${PORT}`)
 });
